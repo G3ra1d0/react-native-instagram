a5c7472c13911d5a7d6e70f669850731b2f84b47
G3ra1d0 <geraldo-vitor@hotmail.com>
2020-05-14 12:11:05 -0400

    Criando Componente Post

--- App.js
+++ App.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import Header from './src/components/Header';
+import Post from './src/components/Post';
+import {View, Text} from 'react-native';
 
 export default () => {
-  return <Header></Header>;
+  return (
+    <View style={{flex: 1}}>
+      <Header />
+      <Post image={require('./assets/imgs/fence.jpg')} />
+    </View>
+  );
 };
