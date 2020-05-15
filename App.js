37df0c6a44abf056797e74a2f0d2942288b4e60c
G3ra1d0 <geraldo-vitor@hotmail.com>
2020-05-15 09:11:59 -0400

    Add Icons

--- App.js
+++ App.js
@@ -1,13 +1,31 @@
 import React from 'react';
 import Header from './src/components/Header';
 import Post from './src/components/Post';
-import {View, Text} from 'react-native';
+import {View} from 'react-native';
 
 export default () => {
+  const comentarios = [
+    {
+      nickname: 'Igor Matheus',
+      comentario: 'Viva o Java!',
+    },
+    {
+      nickname: 'Anderson Silva',
+      comentario: 'Dart é melhor!',
+    },
+    {
+      nickname: 'Matheus Cirino',
+      comentario: 'Paz e Amor!',
+    },
+  ];
+
   return (
     <View style={{flex: 1}}>
       <Header />
-      <Post image={require('./assets/imgs/fence.jpg')} />
+      <Post
+        image={require('./assets/imgs/fence.jpg')}
+        comentarios={comentarios}
+      />
     </View>
   );
 };
