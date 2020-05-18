512a1011f5f727e87b579d92fd8a1dfbd022ca47
G3ra1d0 <geraldo-vitor@hotmail.com>
2020-05-18 09:14:08 -0400

    Colocando usuario logado no header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import {StyleSheet, Text, View, Platform, Image} from 'react-native';
+import {useSelector} from 'react-redux';
 import icon from '../../assets/imgs/icon.png';
 
 export default () => {
+  const user = useSelector((state) => state.user);
+
   return (
     <View style={styles.container}>
       <View style={styles.rowContainer}>
         <Image source={icon} style={styles.image}></Image>
         <Text style={styles.title}>Clone Instagram</Text>
+      </View>
+      <View style={styles.userContainer}>
+        <Text style={styles.user}>{user.name}</Text>
+        <Image
+          source={{
+            uri: user.photoUrl,
+          }}
+          style={styles.avata}></Image>
       </View>
     </View>
   );
@@ -19,6 +30,9 @@ const styles = StyleSheet.create({
     padding: 10,
     borderBottomWidth: 1,
     borderColor: '#BBB',
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
   },
   rowContainer: {
     flexDirection: 'row',
@@ -34,5 +48,19 @@ const styles = StyleSheet.create({
     fontFamily: 'Shelter',
     height: 30,
     fontSize: 28,
+  },
+  userContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  user: {
+    fontSize: 15,
+    color: '#888',
+    marginRight: 5,
+  },
+  avata: {
+    width: 30,
+    height: 30,
+    borderRadius: 15,
   },
 });
